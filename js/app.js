46415babec01905dd3c232b9642c95c530f6105c
Meritdike <dike.merit@yahoo.com>
2022-06-23 15:16:16 +0100

    Zuri task on introduction to javascript

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let fullName = 'Dike Merit'
+let height = '5.7ft'
+let country = 'Nigeria'
+
+
+document.write('Name: ' + fullName + "<br>" + 'Height: ' + height + "<br>" + 'Country: ' + country);
+
+console.log(fullName);
+console.log(height);
+console.log(country);
